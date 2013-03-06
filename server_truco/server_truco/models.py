b31b13734634458ee0f0fff9b0361db89e69d17b
antonio <antonio@pc-antonio.(none)>
2013-03-06 17:03:32 +0100

    django server

---
+++ server_truco/server_truco/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    pass
+class Product(models.Model):
+	pass
+class Store (models.Model):
+	pass
+class Localization(models.Model):
+	pass
