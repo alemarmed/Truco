275679acc3522a11f54164ca434b7e2b4221212a
Alejandro Martin Medina <alejandro@alejandro-PC.(none)>
2013-03-06 23:52:05 +0100

    Creados directorios base del proyecto django

--- server_truco/urls.py
+++ server_truco/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'truco_django.views.home', name='home'),
-    # url(r'^truco_django/', include('truco_django.foo.urls')),
+    url(r'^$', 'server_truco.views.home', name='home'),
+    url(r'^my-stores$', "server_truco.views.list_stores", name="list_stores"),
+    url(r'^store/(?P<id>\w+)/', "server_truco.views.store_form", name="store_form"),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
