6fc0125bdf5cd278f08b25c20e7129d99303ae34
Alejandro Martin <Alejandro Martin>
2013-03-11 18:34:06 +0100

    Markers added to home map

--- server_truco/settings.py
+++ server_truco/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'NAME': 'truco',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'pollollo',
+        'PASSWORD': 'root',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
